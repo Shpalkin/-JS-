883588661701eafc24a2c9d61b8f0ea40e43815b
Maria Bugaeva <116347809+Shpalkin@users.noreply.github.com>
2023-04-04 22:44:34 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,20 +1,14 @@
-let progress = document.getElementById("progress");
-    val = progress.value;
+const progressBar = document.querySelector('progress')
 
 
-
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let response = new XMLHttpRequest();
-
-    response.addEventListener("readystatechange", () => {
-        let ind = response.readyState;
-        progress.value = 1 / (4/ind);
-    });
-    
-    response.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
-    from = document.getElementById("form");
-    data  = new FormData(form);
-    response.send(data);
-});
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = document.getElementById('form');
+    const formData = new FormData(form)
+    const postFile = new XMLHttpRequest()
+    postFile.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload')
+    postFile.upload.onprogress = function(event){
+        progressBar.value = Number(event.total) / Number(event.loaded);
+    }
+    postFile.send(formData)
+})
